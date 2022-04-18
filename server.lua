c253c4bc6abcdf71e4051354f3ca447df5950828
Lauren <95940675+enesep@users.noreply.github.com>
2022-04-18 23:11:38 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,11 +1,14 @@
-local Framework = nil 
+-- By Lauren.
+-- Coded for Lauren Development discord server.
 
-TriggerEvent("Framework:GetObject", function(obj) Framework = obj end)  
+local QBCore = nil 
+
+TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)  
 ------ARAÇ SATMA ----------
 
-Framework.Commands.Add("aracsat", "Araç Satma", {{name="id", help="Oyuncu ID"},  {name="plaka", help="Plaka"}}, false, function(source, args)
+QBCore.Commands.Add("aracsat", "Araç Satma", {{name="id", help="Oyuncu ID"},  {name="plaka", help="Plaka"}}, false, function(source, args)
 	if (args[1] ~= nil or args[2] ~= nil ) then
-		local Player = Framework.Functions.GetPlayer(tonumber(args[1])) -- oyuncu id
+		local Player = QBCore.Functions.GetPlayer(tonumber(args[1])) -- oyuncu id
 		local plaka = args[2] -- plaka
 		if Player ~= nil then
 			if Player.PlayerData.source ~= source then
@@ -26,22 +29,21 @@ RegisterNetEvent("aracsatma:server")
 AddEventHandler("aracsatma:server", function(source,karsi,plate,model)
     local src = source
     local target = karsi.PlayerData.source
-    local Player = Framework.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayer(src)
     local tCid = karsi.PlayerData.citizenid
     local isim = karsi.PlayerData.charinfo.firstname.." "..karsi.PlayerData.charinfo.lastname -- karşı oyuncu ismi
     local ismim = Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname -- kendi  ismi
-	Framework.Functions.ExecuteSql(false, "SELECT * FROM `characters_vehicles` WHERE `citizenid` = '"..Player.PlayerData.citizenid.."' AND `plate` = '"..plate.."'", function(result)
+	QBCore.Functions.ExecuteSql(false, "SELECT * FROM `characters_vehicles` WHERE `citizenid` = '"..Player.PlayerData.citizenid.."' AND `plate` = '"..plate.."'", function(result)
         if result[1] ~= nil then
-				if result[1].citizenid	== Player.PlayerData.citizenid then
-				
-					TriggerClientEvent("Framework:Notify", src,"Aracı başarıyla "..isim.." isimli kişiye sattınız!","success") -- satan kişiye gelen bildirim
-					TriggerClientEvent("Framework:Notify", target,""..ismim.." isimli kişi size "..plate.." Plakalı aracı sattı!","success") -- alan kişiye gelen bildirim
-					Framework.Functions.ExecuteSql(true, "UPDATE `characters_vehicles` SET citizenid='"..tCid.."' WHERE `citizenid` = '"..Player.PlayerData.citizenid.."' AND `plate` = '"..plate.."'")
-					else	
-					TriggerClientEvent("Framework:Notify", src,"Bu aracın sahibi siz değilsiniz!","error") --araç sahibi değilse
-				end
-				else
-				TriggerClientEvent("Framework:Notify", src,"Böyle bir plakalı araca sahip değilsin!","error")
+			if result[1].citizenid	== Player.PlayerData.citizenid then		
+				TriggerClientEvent("QBCore:Notify", src,"Aracı başarıyla "..isim.." isimli kişiye sattınız!","success") -- satan kişiye gelen bildirim
+				TriggerClientEvent("QBCore:Notify", target,""..ismim.." isimli kişi size "..plate.." Plakalı aracı sattı!","success") -- alan kişiye gelen bildirim
+				QBCore.Functions.ExecuteSql(true, "UPDATE `characters_vehicles` SET citizenid='"..tCid.."' WHERE `citizenid` = '"..Player.PlayerData.citizenid.."' AND `plate` = '"..plate.."'")
+			else	
+				TriggerClientEvent("QBCore:Notify", src,"Bu aracın sahibi siz değilsiniz!","error") --araç sahibi değilse
 			end
+		else
+			TriggerClientEvent("QBCore:Notify", src,"Böyle bir plakalı araca sahip değilsin!","error")
+		end
 	end)
 end)
