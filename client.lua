19ef28c6c68624a6057205f8e698c64bc255ae2a
Lauren <95940675+enesep@users.noreply.github.com>
2022-04-18 23:10:32 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,13 @@
-local Framework = nil
+-- By Lauren.
+-- Coded for Lauren Development discord server.
+
+local QBCore = nil
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(10)
-        if Framework == nil then
-            TriggerEvent('Framework:GetObject', function(obj) Framework = obj end)
+        if QBCore == nil then
+            TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
             Citizen.Wait(200)
         end
     end
@@ -18,22 +21,22 @@ AddEventHandler('aracsatma:client', function(kisi,karsi,plaka)
     local sofor = GetPedInVehicleSeat(veh, -1)
     local plate = GetVehicleNumberPlateText(veh)
     local model = veh
-    local Player, Distance = Framework.Functions.GetClosestPlayer()
+    local Player, Distance = QBCore.Functions.GetClosestPlayer()
 	if veh then
 		if plaka == plate then
 			if sofor == ped then
 				  if Player ~= -1 and Distance < 5 and GetPlayerServerId(Player) == karsi.PlayerData.source then  
 					TriggerServerEvent("aracsatma:server",kisi,karsi,plate,model)
 				else
-				Framework.Functions.Notify('Kişi yakınında olmalı!', 'error')
+					QBCore.Functions.Notify('Kişi yakınında olmalı!', 'error')
 				end
-				else	
-				Framework.Functions.Notify('Şöför koltuğunda olman gerekir!', 'error')
-				end
-				else
-				Framework.Functions.Notify('Plaka bulunamadı!', 'error')
+			else	
+				QBCore.Functions.Notify('Şöför koltuğunda olman gerekir!', 'error')
+			end
+		else
+			QBCore.Functions.Notify('Plaka bulunamadı!', 'error')
 		end
-				else	
-				Framework.Functions.Notify('Araçta olman gerekiyor!', 'error')
+	else	
+		QBCore.Functions.Notify('Araçta olman gerekiyor!', 'error')
 	end
 end)
